77d796337f2398cd1fdbc9ffaec867fd772aa4a5
aviralsingh108 <admiralaviral@gmail.com>
2022-04-07 01:26:48 +0530

    RefreshToken implemented, two servers architecture, logout enabled

--- server.js
+++ server.js
@@ -1,42 +1,35 @@
 require("dotenv").config();
+
 const express = require("express");
 const app = express();
-app.use(express.json());
 const jwt = require("jsonwebtoken");
+
+app.use(express.json());
 
 const posts = [
   {
     username: "avi",
-    post: '1'
+    post: "1",
   },
   {
     username: "singh",
-    post: '2'
+    post: "2",
   },
 ];
 
 app.get("/posts", authenticationToken, (req, res) => {
-console.log('req.user', req.user)
-  res.json(posts.filter(post => post.username === req.user.name));
-});
-
-app.post("/login", (req, res) => {
-  console.log('body', req.body)
-  const username = req.body.username;
-  const user = { name: username };
-  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
-  res.json({ acessToken: accessToken });
+  res.json(posts.filter((post) => post.username === req.user.name));
 });
 
 function authenticationToken(req, res, next) {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
   if (token === null) return res.sendStatus(401);
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user)=>{
-      if(err) return res.sendStatus(403)
-      req.user = user
-      next()
-  })
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
 }
 
 app.listen(3000);
